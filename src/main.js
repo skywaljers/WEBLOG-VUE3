3427b6b308262a45d91f718a3da06931a6efd53b
skywaljers <2648760447@qq.com>
2024-04-14 18:30:26 +0800

    登录界面开发

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import '@/assets/main.css'
-
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import { createApp } from 'vue'
 import App from '@/App.vue'
+import 'animate.css'
 
 //导入路由
 import router from '@/router'
@@ -10,3 +11,7 @@ const app = createApp(App)
 //应用路由
 app.use(router)
 app.mount('#app')
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
