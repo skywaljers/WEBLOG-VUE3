07c5114c5c043279b393114a06399ff92cc0d213
skywaljers <2648760447@qq.com>
2025-04-05 13:16:07 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import vue from '@vitejs/plugin-vue'
 import autoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import ViteCompressionPlugin from 'vite-plugin-compression'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,7 +16,14 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()]
-    })
+    }),
+    ViteCompressionPlugin({
+      filter: /\.(js|css)$/i, // 只压缩 js 和 css 文件
+      threshold: 1024, // 只压缩大于 1KB 的文件
+      algorithm: 'gzip', // 使用 gzip 算法
+      ext: '.gz' // 压缩文件的扩展名
+    }),
+    visualizer() // 放到最后
   ],
 
   //代理解决跨域问题
